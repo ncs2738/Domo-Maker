1634b3420f8a63899467b472b80ca8e5301472e4
ncs2738 <46905129+ncs2738@users.noreply.github.com>
2020-04-03 21:33:14 -0400

    Add files via upload

--- server/router.js
+++ server/router.js
@@ -6,6 +6,7 @@ const router = (app) => {
   app.get('/signup', controllers.Account.signupPage);
   app.post('/signup', controllers.Account.signup);
   app.get('/maker', controllers.Domo.makerPage);
+  app.post('/maker', controllers.Domo.make);
   app.get('/logout', controllers.Account.logout);
   app.get('/', controllers.Account.loginPage);
 };
